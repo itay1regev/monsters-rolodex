bdc0f0e586402c2e7d4f6fd30aca968b1cccca8a
itay1regev <30416897+itay1regev@users.noreply.github.com>
2020-04-13 00:03:32 +0300

    adding files for github pages

---
+++ src/exercises/ex_event_binding.jsx
@@ -0,0 +1,31 @@
+import ReactDom from "react-dom";
+
+class App extends React.Component {
+ constructor(){
+     super();
+     this.handleClick2= this.handleClick1.bind(this)
+ }   
+ 
+ handleClick1() {
+     console.log('button 1 clicked');
+ }
+ 
+ handleClick3 = () => console.log('button 3 clicked');
+
+render() {
+    return (
+        <div>
+
+            <button onClick= {this.handleClick1()}>click 1</button>
+            <button onClick= {this.handleClick1}>click 2 </button>
+            <button onClick={this.handleClick2}>click 3</button>
+            <button onClick={this.handleClick3}>click 4</button>
+        </div>
+
+    )
+
+}
+
+
+
+}
